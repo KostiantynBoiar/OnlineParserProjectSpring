f4f3fa1bab76899ae3592dc66499ab869e1352e1
opezdalya <opezdalya@gmail.com>
2024-04-16 21:46:31 +0100

    Configure application.properties file

--- src/main/java/com/example/onlinewebscrapperspring/OnlineWebScrapperSpringApplication.java
+++ src/main/java/com/example/onlinewebscrapperspring/OnlineWebScrapperSpringApplication.java
@@ -2,8 +2,10 @@ package com.example.onlinewebscrapperspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class OnlineWebScrapperSpringApplication {
 
     public static void main(String[] args) {
