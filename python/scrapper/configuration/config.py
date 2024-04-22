bb03899869f20d52bb8603f5ae555856e2d900c4
opezdalya <opezdalya@gmail.com>
2024-04-22 11:45:58 +0100

    Created python side of the application

---
+++ python/scrapper/configuration/config.py
@@ -0,0 +1,19 @@
+from configparser import ConfigParser
+
+def load_config(filename='database.ini', section='postgresql'):
+    parser = ConfigParser()
+    parser.read(filename)
+
+    config = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            config[param[0]] = param[1]
+    else:
+        raise Exception(f'Section {section} not found in the {filename} file')
+
+    return config
+
+if __name__ == '__main__':
+    load_config()
+    print(load_config)
---
+++ python/scrapper/rozetka_scrapper.py
@@ -0,0 +1,7 @@
+from bs4 import BeautifulSoup
+import requests
+import psycopg2
+def scrapper():
+
+if __name__ == '__main__':
+    scrapper()
