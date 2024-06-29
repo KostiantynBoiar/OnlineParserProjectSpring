3611c506d8ece22fc6c4603314ccc93b24f5c35d
opezdalya <opezdalya@gmail.com>
2024-06-29 11:08:26 +0100

    Create ElasticSearch Microservice

---
+++ src/main/java/com/example/onlinewebscrapperspring/Microservices/ElasticMicroservice/src/main/java/com/scrapper/elasticmicroservice/ElasticMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.scrapper.elasticmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/onlinewebscrapperspring/Microservices/ElasticMicroservice/src/test/java/com/scrapper/elasticmicroservice/ElasticMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scrapper.elasticmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
