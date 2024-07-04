19e8f66dc6118eac01c5e1bff86031f327859903
opezdalya <opezdalya@gmail.com>
2024-07-04 18:02:19 +0100

    Create controller

--- src/main/java/com/example/onlinewebscrapperspring/Microservices/ElasticMicroservice/src/main/java/com/scrapper/elasticmicroservice/Configuration/ElasticMicroserviceConfiguration.java
+++ src/main/java/com/example/onlinewebscrapperspring/Microservices/ElasticMicroservice/src/main/java/com/scrapper/elasticmicroservice/Configuration/ElasticMicroserviceConfiguration.java
@@ -1,4 +1,9 @@
 package com.scrapper.elasticmicroservice.Configuration;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories
 public class ElasticMicroserviceConfiguration {
 }
--- src/main/java/com/example/onlinewebscrapperspring/Microservices/ElasticMicroservice/src/main/java/com/scrapper/elasticmicroservice/Controller/ElasticController.java
+++ src/main/java/com/example/onlinewebscrapperspring/Microservices/ElasticMicroservice/src/main/java/com/scrapper/elasticmicroservice/Controller/ElasticController.java
@@ -1,7 +1,24 @@
 package com.scrapper.elasticmicroservice.Controller;
 
+import com.scrapper.elasticmicroservice.Model.ProductModel;
+import com.scrapper.elasticmicroservice.Service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/elastic/products/")
 public class ElasticController {
+
+        @Autowired
+        private ProductService productService;
+
+        @GetMapping("")
+        public List<ProductModel> getAllProducts() {
+            return productService.getAllProducts();
+        }
 }
