b805406e9c1eb358ffe49c795b534bcb19fc8a02
opezdalya <opezdalya@gmail.com>
2024-07-04 17:56:41 +0100

    Fix of repository

--- src/main/java/com/example/onlinewebscrapperspring/Microservices/ElasticMicroservice/src/main/java/com/scrapper/elasticmicroservice/Repository/ProductRepository.java
+++ src/main/java/com/example/onlinewebscrapperspring/Microservices/ElasticMicroservice/src/main/java/com/scrapper/elasticmicroservice/Repository/ProductRepository.java
@@ -1,7 +1,10 @@
 package com.scrapper.elasticmicroservice.Repository;
 
+import com.scrapper.elasticmicroservice.Model.ProductModel;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ProductRepository {
+public interface ProductRepository extends ElasticsearchRepository<ProductModel, Long> {
+
 }
--- src/main/java/com/example/onlinewebscrapperspring/Microservices/ElasticMicroservice/src/main/java/com/scrapper/elasticmicroservice/Service/ProductService.java
+++ src/main/java/com/example/onlinewebscrapperspring/Microservices/ElasticMicroservice/src/main/java/com/scrapper/elasticmicroservice/Service/ProductService.java
@@ -1,7 +1,14 @@
 package com.scrapper.elasticmicroservice.Service;
 
+import com.scrapper.elasticmicroservice.Model.ProductModel;
+import com.scrapper.elasticmicroservice.Repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
+
+
 }
